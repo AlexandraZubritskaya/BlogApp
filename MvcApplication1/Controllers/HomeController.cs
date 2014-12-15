d8f9d07ab5d4573452176a40f7d1b54a95678c98
Alexandra <saska1345@mail.ru>
2014-12-15 23:51:15 +0400

    Второй способ работы с БД

--- MvcApplication1/Controllers/HomeController.cs
+++ MvcApplication1/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using MvcApplication1.Models;
 using MvcApplication1.Models.Repository;
+using System.Collections.ObjectModel;
+
 
 namespace MvcApplication1.Controllers
 {
@@ -29,7 +31,7 @@ namespace MvcApplication1.Controllers
        // {
        //     return RedirectToAction("index", "home");
         //}
-        [HttpGet]
+        /*[HttpGet]
         public ActionResult Index(string title)
         {
             //string query = Request.QueryString["Foo"]; // Запрос в  адресной строке после знака?
@@ -56,6 +58,57 @@ namespace MvcApplication1.Controllers
                 //CommentRepository.Comments.Add(model.NewComment.Comment);
                 ModelState.Clear();
                 return View(readers.GetHistory(title));
+            }
+
+            return View(model);
+        }*/
+         [HttpGet]
+        public ActionResult Index(string title)
+        {
+            
+            if (title == null)
+            {
+                title = "А. де Сент Экзепюри «Маленький принц»";
+            }
+            using (var ctx = new EFContext())
+            {
+                var post = ctx.Posts.Where(p => p.Title == title).FirstOrDefault();
+                var postModel = new PostModel(post.Title, post.Body, post.DateCreated);
+                var commentModel = new Collection<string>();
+                if (post.Comments != null && post.Comments.Any())
+                {
+                    foreach (var item in post.Comments)
+                    {
+                        commentModel.Add(item.Body);
+                    }
+                }
+
+                return View(new History(postModel, commentModel));
+            }
+            
+        }
+
+        [HttpPost]
+        //[ValidateInput(false)] // можно вводить html теги
+        public ActionResult Index(History model)
+       
+        {
+            var title = "А. де Сент Экзепюри «Маленький принц»";
+            
+            if (model.NewComment != null && ModelState.IsValid)
+            
+            {
+                using (var ctx = new EFContext())
+                {
+                    var post = ctx.Posts.Where(p => p.Title == title).FirstOrDefault();
+                    if (post != null)
+                    {
+                        ctx.Comments.Add(new Comment() { Body = model.NewComment.Comment, PostID = post.PostID });
+                        ctx.SaveChanges();
+                    }
+                }
+                ModelState.Clear();
+                return RedirectToAction("Index", new { title = title });
             }
 
             return View(model);
